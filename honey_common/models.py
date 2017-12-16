4fe6d0249bf8d5829f2a3535024feae6b4f742f6
yoon <fromleaf@gmail.com>
2017-12-16 14:00:41 +0900

    add common model because have to remain model when they are deleted.

--- honey_common/models.py
+++ honey_common/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from honey_common import utils
+
+
+class CommonModel(models.Model):
+    """
+    Default Model.
+    All models aren't removed on database, but they look like to be removed.
+    """
+    is_deleted = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted = utils.get_local_today()
+        self.save()
